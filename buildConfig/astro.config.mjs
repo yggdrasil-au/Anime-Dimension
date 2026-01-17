fc9e1e1f2aad2cb032e9b132b7de4c119fba904e
samarixum <66809702+samarixum@users.noreply.github.com>
2026-01-17 16:22:19 +1100

    [v0.10.3] changes: 4
    Files changed in this commit:
    	modified:   buildConfig/astro.config.mjs
    	modified:   package.json
    	modified:   scripts.yaml
    	modified:   subModules/Anime-Dimension-api

--- buildConfig/astro.config.mjs
+++ buildConfig/astro.config.mjs
@@ -2,7 +2,7 @@
 
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-import node from '@astrojs/node';
+//import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
